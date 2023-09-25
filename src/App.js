704e36cc48d2be3b220790e9bbb3c36a74075a35
Tushar <mathurtushar2002@gmail.com>
2023-09-25 11:47:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
-import Loginpage from './components/Pages/Loginpage';
 import Heropage from './components/Heropage';
 import Coursesec from './components/Coursesec';
 import Plaformdes from './components/Plaformdes';
@@ -9,19 +8,12 @@ import Testimonial from './components/Testimonial';
 import Sponsor from './components/Sponsor';
 import Cardsec from './components/Cardsec';
 import Footer from './components/Footer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 export default function App() {
   return (
     <>
     <div>  
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          <Route path="login" element={<Loginpage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
       <Navbar/>
       <Heropage/>
       <Coursesec/>
