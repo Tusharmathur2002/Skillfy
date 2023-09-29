2954c92fe22b9eea86882d76aafad6f2495cf3ad
Tushar <mathurtushar2002@gmail.com>
2023-09-29 11:49:21 +0530

    connecting with the server

--- src/components/Pages/Register.js
+++ src/components/Pages/Register.js
@@ -3,11 +3,16 @@ import './Login.css'
 
 
 export default function Register() {
+
+  const back = async (e) => {
+    const response = await fetch('http://localhost:3001/')
+    
+  }
   return (
     <section className="body1">
       <div className="container1" >
         <h1>Login</h1>
-        <form action=''>
+        <form onSubmit={back}>
           <div className="box">
             <i className="fa-solid fa-envelope"></i>
             <input type="text" name="email" id="email" placeholder="Example@email.com" />
@@ -16,7 +21,7 @@ export default function Register() {
             <i className="fa-solid fa-key"></i>
             <input type="password" name="password" id="password" placeholder="Enter Password" />
           </div>
-          <button className="btn1">Sign-Up</button>
+          <button className="btn1" type='submit' >Sign-Up</button>
         </form>
       </div>
     </section >
